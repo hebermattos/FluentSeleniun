11f20c4d013e3432e23c3575150ebf727f208566
Heber Mattos <heber.mattos@ilegra.com>
2019-12-13 09:49:23 -0300

    error

--- Example/Program.cs
+++ Example/Program.cs
@@ -9,28 +9,19 @@ namespace Seleniun.Example
         static void Main()
         {
             var seleniun = new FluentSeleniun(
-                  browserType: SeleniumBrowserType.Firefox,
+                  browserType: SeleniumBrowserType.Chrome,
                   snapShotPath: "c:\\Selenium_SnapShots\\",
                   driverFolderPath: Directory.GetCurrentDirectory(),
-                  maxWaitSeconds: 300);
+                  maxWaitSeconds: 3);
 
-            try
-            {
-                seleniun
-                    .OpenUrl("https://www.google.com/")
-                    .PageTitleShoulBe("Google")
-                    .FillByName("q", "devops")
-                    .ClickButtonByName("btnK")
-                    .PageTitleShoulContains("devops"); 
-            }
-            catch (Exception ex)
-            {
-                seleniun.SavePrint(ex);
-            }
-            finally
-            {
-                seleniun.CloseBrowser();
-            }
+
+            seleniun
+                .OpenUrl("https://www.google.com/")
+                .PageTitleShoulBe("Google")
+                .FillByName("q", "devops")
+                .ClickButtonByName("btnK")
+                .PageTitleShoulContains("devops")
+                .Run();
         }
     }
 }
--- src/FluentSeleniun.cs
+++ src/FluentSeleniun.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using OpenQA.Selenium.Support.UI;
 using ExpectedConditions = SeleniumExtras.WaitHelpers.ExpectedConditions;
 
@@ -10,151 +12,208 @@ namespace OpenQA.Selenium
         private readonly IWebDriver _browser;
         private readonly WebDriverWait _wait;
         private readonly string _snapShotPath;
+        private List<Action> _methods;
 
-        private FluentSeleniun(string snapShotPath)
-        {
-            _snapShotPath = snapShotPath;
-        }
-
-        public FluentSeleniun(IWebDriver webDriver, string snapShotPath, int maxWaitSeconds) : this(snapShotPath)
-        {
-            _browser = webDriver;
-            _wait = new WebDriverWait(_browser, TimeSpan.FromSeconds(maxWaitSeconds));
-            _wait.PollingInterval = TimeSpan.FromMilliseconds(100);
-        }
-
-        public FluentSeleniun(SeleniumBrowserType browserType, string snapShotPath, string driverFolderPath, int maxWaitSeconds) : this(snapShotPath)
+        public FluentSeleniun(SeleniumBrowserType browserType, string snapShotPath, string driverFolderPath, int maxWaitSeconds)
         {
             _browser = SeleniumBrowserFactory.Get(browserType, driverFolderPath);
             _wait = new WebDriverWait(_browser, TimeSpan.FromSeconds(maxWaitSeconds));
+            _wait.PollingInterval = TimeSpan.FromMilliseconds(100);
+            _snapShotPath = snapShotPath;
+            _methods = new List<Action>();
         }
 
         public FluentSeleniun OpenUrl(string url)
         {
-            _browser.Navigate().GoToUrl(url);
+            _methods.Add(() =>
+            {
+                _browser.Navigate().GoToUrl(url);
+            });
 
             return this;
         }
 
         public FluentSeleniun PageTitleShoulBe(string text, StringComparison stringComparison = StringComparison.InvariantCultureIgnoreCase)
         {
-            if (!_browser.Title.Equals(text, stringComparison))
-                throw new Exception($"Page title should be {text}");
+            _methods.Add(() =>
+            {
+                if (!_browser.Title.Equals(text, stringComparison))
+                    throw new Exception($"Page title should be {text}");
+            });
 
             return this;
         }
 
         public FluentSeleniun PageTitleShoulContains(string text)
         {
-            if (!_browser.Title.Contains(text))
-                throw new Exception($"Page title should contains {text}");
+            _methods.Add(() =>
+            {
+                if (!_browser.Title.Contains(text))
+                    throw new Exception($"Page title should contains {text}");
+            });
 
             return this;
         }
 
         public FluentSeleniun PageShouldContains(string text)
         {
-            if (!_browser.PageSource.Contains(text))
-                throw new Exception($"Page should contains {text}");
+            _methods.Add(() =>
+            {
+                if (!_browser.PageSource.Contains(text))
+                    throw new Exception($"Page should contains {text}");
+            });
 
             return this;
         }
 
         public FluentSeleniun ClickAlert()
         {
-            _wait.Until(AlertExists);
-            _browser.SwitchTo().Alert().Accept();
+            _methods.Add(() =>
+            {
+                _wait.Until(AlertExists);
+                _browser.SwitchTo().Alert().Accept();
+            });
 
             return this;
         }
 
         public FluentSeleniun ClickButtonByName(string name)
         {
-            GetByName(name).Click();
+            _methods.Add(() =>
+            {
+                GetByName(name).Click();
+            });
 
             return this;
         }
 
         public FluentSeleniun ClickButtonById(string id)
         {
-            GetById(id).Click();
+            _methods.Add(() =>
+            {
+                GetById(id).Click();
+            });
 
             return this;
         }
 
         public FluentSeleniun ClickButtonByXPath(string xpath)
         {
-            GetByXPath(xpath).Click();
+            _methods.Add(() =>
+            {
+                GetByXPath(xpath).Click();
+            });
 
             return this;
         }
 
         public FluentSeleniun FillByName(string name, string value)
         {
-            var element = GetByName(name);
-            element.Clear();
-            element.SendKeys(value);
+            _methods.Add(() =>
+            {
+                var element = GetByName(name);
+                element.Clear();
+                element.SendKeys(value);
+            });
 
             return this;
         }
 
         public FluentSeleniun FillById(string id, string value)
         {
-            var element = GetById(id);
-            element.Clear();
-            element.SendKeys(value);
+            _methods.Add(() =>
+            {
+                var element = GetById(id);
+                element.Clear();
+                element.SendKeys(value);
+            });
 
             return this;
         }
 
         public FluentSeleniun FillByXPath(string xpath, string value)
         {
-            var element = GetByXPath(xpath);
-            element.Clear();
-            element.SendKeys(value);
+            _methods.Add(() =>
+            {
+                var element = GetByXPath(xpath);
+                element.Clear();
+                element.SendKeys(value);
+            });
 
             return this;
         }
 
         public FluentSeleniun SelectDdlByName(string name, string value)
         {
-            var element = GetByName(name);
-            new SelectElement(element).SelectByText(value);
+            _methods.Add(() =>
+            {
+                var element = GetByName(name);
+                new SelectElement(element).SelectByText(value);
+            });
 
             return this;
         }
 
         public FluentSeleniun SelectDdlById(string id, string value)
         {
-            var element = GetById(id);
-            new SelectElement(element).SelectByText(value);
+            _methods.Add(() =>
+            {
+                var element = GetById(id);
+                new SelectElement(element).SelectByText(value);
+            });
 
             return this;
         }
 
         public FluentSeleniun SelectDdlByXPath(string xpath, string value)
         {
-            var element = GetByXPath(xpath);
-            new SelectElement(element).SelectByText(value);
+            _methods.Add(() =>
+           {
+               var element = GetByXPath(xpath);
+               new SelectElement(element).SelectByText(value);
+           });
 
             return this;
         }
 
-        public FluentSeleniun CloseBrowser()
+
+        public void Run()
         {
-            _browser.Quit();
-
-            return this;
+            try
+            {
+                foreach (var method in _methods)
+                {
+                    method.Invoke();
+                }
+            }
+            catch (Exception ex)
+            {
+                SavePrint(ex);
+            }
+            finally
+            {
+                CloseBrowser();
+            }
         }
 
-        public void SavePrint(Exception erro)
+        private void SavePrint(Exception erro)
         {
             Directory.CreateDirectory(_snapShotPath);
 
             var screnn = (ITakesScreenshot)_browser;
 
             Screenshot print = screnn.GetScreenshot();
-            print.SaveAsFile(_snapShotPath + erro.Message.Replace(" ", "_") + ".png", ScreenshotImageFormat.Png);
+            print.SaveAsFile(_snapShotPath + RemoveSpecialCharacters(erro.Message.Replace(" ", "_")) + ".png", ScreenshotImageFormat.Png);
+        }
+
+        private static string RemoveSpecialCharacters(string str)
+        {
+            var sb = new StringBuilder();
+            foreach (char c in str)
+                if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '.' || c == '_')
+                    sb.Append(c);
+
+            return sb.ToString();
         }
 
         private IWebElement GetByName(string name)
@@ -186,5 +245,11 @@ namespace OpenQA.Selenium
                 return false;
             }
         }
+
+        private void CloseBrowser()
+        {
+            _browser.Quit();
+        }
+
     }
 }
